0aff3a4bc937e64e52c2708fc9933019a0cb935e
amuqeet0 <abdul.muqeet@codefulcrum.com>
2022-08-31 14:35:11 +0500

    old clinet src removed

--- client/src/screens/App/shared/components/Footer/Footer.js
+++ client/src/screens/App/shared/components/Footer/Footer.js
@@ -1,7 +1,7 @@
-import React from 'react';
-import styles from './Footer.module.scss';
+import React from "react";
+import styles from "./Footer.module.scss";
 
-const Footer = props => {
+const Footer = (props) => {
   const { altColors = false } = props;
   return (
     <div
@@ -9,24 +9,16 @@ const Footer = props => {
         altColors === false
           ? `${styles.footer}`
           : `${styles.footer} ${styles.footerAltColors}`
-      }>
-      <div>
-        Currently maintained by&nbsp;
-        <a
-          title="My GitHub user page"
-          href="https://github.com/arkn98"
-          target="_blank"
-          rel="noopener noreferrer">
-          Arun Kumar
-        </a>
-      </div>
+      }
+    >
       <div>|</div>
       <div>
         <a
           title="GitHub repo"
-          href="https://github.com/arkn98/leave-management-system"
+          href="https://github.com/amuqeet0/leave-management-system"
           target="_blank"
-          rel="noopener noreferrer">
+          rel="noopener noreferrer"
+        >
           View source code
         </a>
       </div>
@@ -34,20 +26,11 @@ const Footer = props => {
       <div>
         <a
           title="Issue Tracker"
-          href="https://github.com/arkn98/leave-management-system/issues"
+          href="https://github.com/arknamuqeet0/leave-management-system/issues"
           target="_blank"
-          rel="noopener noreferrer">
+          rel="noopener noreferrer"
+        >
           Report an issue
-        </a>
-      </div>
-      <div>|</div>
-      <div>
-        <a
-          title="Submit your feedback"
-          href="https://goo.gl/forms/NP0pqpHuDlRYGoz92"
-          target="_blank"
-          rel="noopener noreferrer">
-          Feedback
         </a>
       </div>
     </div>
